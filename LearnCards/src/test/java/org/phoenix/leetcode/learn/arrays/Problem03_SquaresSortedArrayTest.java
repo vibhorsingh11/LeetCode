13bd1c0484db07e11936af226eb782dd45b34aba
vibhorsingh11 <vibhorsinghcs@gmail.com>
2020-04-30 23:20:39 +0530

    Added Squares of a Sorted Array

---
+++ LearnCards/src/test/java/org/phoenix/leetcode/learn/arrays/Problem03_SquaresSortedArrayTest.java
@@ -0,0 +1,18 @@
+package org.phoenix.leetcode.learn.arrays;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class Problem03_SquaresSortedArrayTest {
+
+    private final Problem03_SquaresSortedArray test = new Problem03_SquaresSortedArray();
+
+    @Test
+    void sortedSquares() {
+
+        int[] arr = new int[]{-4,-1,0,3,10};
+        int[] expected = new int[]{0, 1, 9, 16, 100};
+
+        Assertions.assertEquals(expected, test.sortedSquares(arr));
+    }
+}
