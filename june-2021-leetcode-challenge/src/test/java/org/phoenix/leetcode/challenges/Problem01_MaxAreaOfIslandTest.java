a0a9a853000897959ea13c133ea7b9c9fa07ee73
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-06-01 12:58:20 +0530

    Added Problem 01 Max Area Of Island

---
+++ june-2021-leetcode-challenge/src/test/java/org/phoenix/leetcode/challenges/Problem01_MaxAreaOfIslandTest.java
@@ -0,0 +1,25 @@
+package org.phoenix.leetcode.challenges;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class Problem01_MaxAreaOfIslandTest {
+
+    private final Problem01_MaxAreaOfIsland test = new Problem01_MaxAreaOfIsland();
+
+    @Test
+    void maxAreaOfIsland() {
+        int[][] grid = new int[][]{
+                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 0, 1, 0, 0},
+                {0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0},
+                {0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0}
+        };
+        assertEquals(6, test.maxAreaOfIsland(grid));
+    }
+}
