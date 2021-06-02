ac3c049b4ed97594168135ac4c334742c9393e39
vibhorsingh11 <vibhorsinghcs@gmail.com>
2021-06-02 12:59:53 +0530

    updated

--- june-2021-leetcode-challenge/src/main/java/org/phoenix/leetcode/challenges/Problem01_MaxAreaOfIsland.java
+++ june-2021-leetcode-challenge/src/main/java/org/phoenix/leetcode/challenges/Problem01_MaxAreaOfIsland.java
@@ -1,8 +1,30 @@
 package org.phoenix.leetcode.challenges;
 
+/**
+ * You are given an m x n binary matrix grid. An island is a group of 1's (representing land) connected 4-directionally
+ * (horizontal or vertical.) You may assume all four edges of the grid are surrounded by water.
+ * <p>
+ * The area of an island is the number of cells with a value 1 in the island.
+ * <p>
+ * Return the maximum area of an island in grid. If there is no island, return 0.
+ * Constraints:
+ * <p>
+ * m == grid.length
+ * n == grid[i].length
+ * 1 <= m, n <= 50
+ * grid[i][j] is either 0 or 1.
+ */
+
 public class Problem01_MaxAreaOfIsland {
     public int maxAreaOfIsland(int[][] grid) {
+        // Base case
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        // Max area of island
         int max = 0;
+        // Looping over the grid and aplying
+        // DFS on each neighbours
         for (int i = 0; i < grid.length; i++) {
             for (int j = 0; j < grid[0].length; j++) {
                 if (grid[i][j] == 1) {
@@ -13,11 +35,15 @@ public class Problem01_MaxAreaOfIsland {
         return max;
     }
 
-    int dfs(int[][] grid, int i, int j) {
+    private int dfs(int[][] grid, int i, int j) {
+        // Base case
         if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) {
             return 0;
         }
+        // Make the current cell 0 as
+        // we will not encounter it again
         grid[i][j] = 0;
+        // Recursive call on each neighbours
         return 1 + dfs(grid, i - 1, j) + dfs(grid, i, j - 1) + dfs(grid, i, j + 1) + dfs(grid, i + 1, j);
     }
 }
